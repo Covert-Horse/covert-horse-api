bafae0f2a5f3bdcaef9403de59b71a54e06943e0
Isaiah Glass <glass.364@osu.edu>
2026-04-08 10:31:53 -0400

    Lab 5: Added Domain unit tests and test project

---
+++ tests/Covert.Horse.Domain.Tests/RatingTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Covert.Horse.Domain.Catalog;
+
+namespace Covert.Horse.Domain.Tests;
+
+[TestClass]
+public class RatingTests
+{
+    [TestMethod]
+    public void Can_Create_New_Rating()
+    {
+        // Arrange
+        var rating = new Rating(5, "Great fit!");
+
+        // Assert
+        Assert.AreEqual(5, rating.Stars);
+        Assert.AreEqual("Great fit!", rating.Review);
+    }
+
+    [TestMethod]
+    public void Cannot_Create_Rating_With_Invalid_Stars()
+    {
+        // Arrange
+        bool exceptionThrown = false;
+
+        try
+        {
+            var rating = new Rating(0, "Bad rating");
+        }
+        catch (ArgumentException)
+        {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        Assert.IsTrue(exceptionThrown);
+    }
+}
